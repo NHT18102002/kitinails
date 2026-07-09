9ab48644fb8541b08928e5147743b022a2b75e13
Nguyễn Hữu Truyền <truyennh.tomotek@gmail.com>
2026-07-09 23:31:03 +0700

    Fix desktop header dropdown hover

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -28,6 +28,11 @@ class DetailsDisclosure extends HTMLElement {
     this.mainDetailsToggle.removeAttribute('open');
     this.mainDetailsToggle.querySelector('summary').setAttribute('aria-expanded', false);
   }
+
+  open() {
+    this.mainDetailsToggle.setAttribute('open', '');
+    this.mainDetailsToggle.querySelector('summary').setAttribute('aria-expanded', true);
+  }
 }
 
 customElements.define('details-disclosure', DetailsDisclosure);
@@ -36,6 +41,41 @@ class HeaderMenu extends DetailsDisclosure {
   constructor() {
     super();
     this.header = document.querySelector('.header-wrapper');
+    this.setupDesktopHoverMenu();
+  }
+
+  setupDesktopHoverMenu() {
+    this.desktopHoverMedia = window.matchMedia('(min-width: 990px) and (hover: hover) and (pointer: fine)');
+    this.hoverCloseTimer = null;
+
+    this.mainDetailsToggle.addEventListener('mouseenter', this.onDesktopMouseEnter.bind(this));
+    this.mainDetailsToggle.addEventListener('mouseleave', this.onDesktopMouseLeave.bind(this));
+
+    if (this.content) {
+      this.content.addEventListener('mouseenter', this.onDesktopMouseEnter.bind(this));
+      this.content.addEventListener('mouseleave', this.onDesktopMouseLeave.bind(this));
+    }
+  }
+
+  onDesktopMouseEnter() {
+    if (!this.desktopHoverMedia.matches) return;
+
+    window.clearTimeout(this.hoverCloseTimer);
+    document.querySelectorAll('header-menu').forEach((menu) => {
+      if (menu !== this && typeof menu.close === 'function') menu.close();
+    });
+    this.open();
+  }
+
+  onDesktopMouseLeave() {
+    if (!this.desktopHoverMedia.matches) return;
+
+    window.clearTimeout(this.hoverCloseTimer);
+    this.hoverCloseTimer = window.setTimeout(() => {
+      if (!this.mainDetailsToggle.matches(':hover') && !this.contains(document.activeElement)) {
+        this.close();
+      }
+    }, 120);
   }
 
   onToggle() {
